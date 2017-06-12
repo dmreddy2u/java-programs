0f2a28ed7f61e38713f66e79506095baf19baf3b
dmreddy2u <dmreddy2u@gmail.com>
2017-06-11 22:34:21 -0500

    Add files via upload

---
+++ Divisbleby7.java
@@ -0,0 +1,22 @@
+
+public class Divisbleby7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int count = 0;
+		for (int i = 1; i <= 100; i++) {
+
+			{
+				if (i % 7 == 0){
+					System.out.print(i + " ");
+				count++;
+				System.out.println(count);
+			}
+
+		}
+
+	}
+
+	}
+}
